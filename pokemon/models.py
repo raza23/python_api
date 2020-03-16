b3f616cfe5b254458224c7feb592f0226b1a8c66
Raza <rtshareef@gmail.com>
2020-03-16 00:33:49 -0400

    serializer

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- pokemon/views.py
+++ pokemon/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Pokemon
+from .serializers import PokemonSerializer
+
 
 # Create your views here.
+class PokemonView(viewsets.ModelViewSet):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
