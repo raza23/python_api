ceb5e19559cff0f6e7b5669a87d1c44003640d83
Raza <rtshareef@gmail.com>
2020-03-23 22:25:50 -0400

    working

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,8 +1,13 @@
 
 from django.conf.urls import url, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('pokemon', views.PokemonView)
 
 
 urlpatterns = [
+    url('', include(router.urls)),
 
 ]
